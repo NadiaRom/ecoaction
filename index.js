049dc51ade3f6f43de9d33cef32f0dcb498b79d1
Nadiia Romanenko <nadiia.romanenko@gmail.com>
2019-02-22 14:58:18 +0200

    fix io

--- index.js
+++ index.js
@@ -427,7 +427,7 @@ Promise.all([
         const scroller = scrollama();
 
         scroller.setup({
-            step: '#consumption .text_cont',
+            step: '#consumption .text_cont .text',
             container: '#consumption',
             graphic: '#consumption .fig_container',
             offset: 0.85,
